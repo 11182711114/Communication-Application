905cdabb2fc9f9fe0fde9483e1e54e60bb7012aa
Fredrik <flarsson93@gmail.com>
2016-12-14 15:06:39 +0100

    Trying out sockets! :)

--- src/Application/Connection.java
+++ src/Application/Connection.java
@@ -1,5 +1,16 @@
 package Application;
 
-public class Connection {
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
 
+public class Connection {
+	Socket iSocket;
+	
+	public Connection(byte[] adr) throws IOException{
+		InetAddress ip = InetAddress.getByAddress(adr);
+		iSocket = new Socket(ip,8080);
+		iSocket.bind(null);
+		iSocket.close();
+	}
 }
