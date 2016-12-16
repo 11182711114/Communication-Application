2afde15c07ac6d977515a9d9b9e7d63f18c175cd
Axel <lexa.andersson@gmail.com>
2016-12-16 15:49:20 +0100

    Fixed send method in connection2 minor changes in portlistener. basic structure for run.

--- src/Application/Connection2.java
+++ src/Application/Connection2.java
@@ -1,12 +1,17 @@
 
 package Application;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.Socket;
+
+//OBS formatera!
 
 public class Connection2 implements Runnable 
 {
 	private Socket socket;
 	private Channel channel;
+	private boolean run;
 	
 	public Connection2(Socket s, Channel h)
 	{
@@ -16,6 +21,41 @@ public class Connection2 implements Runnable
 	
 	@Override
 	public void run() 
+	{
+		run = true;
+		do
+		{
+			
+		}
+		while(run);
+	}
+	
+	public void closeConnection()
+	{
+		run = false;
+	}
+	
+	public boolean send(String s)
+	{
+		try 
+		{
+			DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+			
+			output.writeUTF(s);
+			output.flush();
+			
+			output.close();
+			return true;
+		} 
+		catch (IOException e)
+		{
+			//Tänk på felhantering!
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public void receive()
 	{
 		
 	}
