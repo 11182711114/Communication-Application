44987ea5759fc5e8a5a18ba714426f6e77459b40
Johan <Johan.Eklundh@gmail.com>
2016-12-16 15:37:23 +0100

    IO = identifiable

--- src/Application/IO.java
+++ src/Application/IO.java
@@ -6,6 +6,20 @@ public class IO implements Runnable{
 	ArrayList<Writer>  writerCollection = new ArrayList<>();	
 	ArrayList<Reader>  readerCollection = new ArrayList<>();
 	
+	
+	private int id = 1; 
+	public static int idCount;
+	private Channel myChannel;
+	
+	
+	public IO(Channel channel){
+		myChannel = channel;
+		idCount = id;
+		this.id =id++;
+		
+	}
+	
+	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
