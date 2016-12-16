61188ca03f90f02e2a66f5688695bc177d783747
Fredrik <flarsson93@gmail.com>
2016-12-16 15:09:45 +0100

    add data storage structure and implement Comparable

--- src/Application/Channel.java
+++ src/Application/Channel.java
@@ -1,19 +1,28 @@
 package Application;
 
-public class Channel {
-	private Connection comID;
+public class Channel implements Comparable<Channel>{
+	private String comID;
 	
-	private Connection con;
+	private Connection2 con;
 	private IO inOut;
 	
 	/**
-	 * Desc desc desc.
-	 * @param  variable Description text text text.
-	 * @param  different_variable text text text.
-	 * @return Description text text text.
+	 * Handles Connections and IO operations.
+	 * @param  Con connection between two devices
+	 * @param  inOut IPC between two processes
 	 */
-	public Channel(){
-		
+	public Channel(Connection2 con, IO inOut){
+		this.con = con;
+		this.inOut = inOut;		
 	}
-
+	public void startChannel(){
+		con.run();
+	}
+	@Override
+	public int compareTo(Channel c) {
+		return comID.equals(c.getComID()) ? 1 : 0;
+	}
+	public String getComID(){
+		return comID;
+	}
 }
--- src/Application/ChannelHandler.java
+++ src/Application/ChannelHandler.java
@@ -1,16 +1,21 @@
 package Application;
 
 import java.util.List;
+import java.util.Set;
 
 public class ChannelHandler {
 	
-	private List<Connection> cons;
+	private List<Channel> channels;
+	private Set<Channel> channelsSet;
 	private PortListener sListener;
 	
-	public ChannelHandler(List<Connection> cons,PortListener sListener){
-		this.cons = cons;
+	public ChannelHandler(Set<Channel> channelsSet,List<Channel> cons,PortListener sListener){
+		this.channels = cons;
 		this.sListener = sListener;
+		this.channelsSet = channelsSet;
 	}
-	
-
+	public void addChannel(Channel c){
+		c.startChannel();
+		channels.add(c);		
+	}
 }
