6e6e335944bf524a9cb5655bb7eb802064912c9c
Fredrik <flarsson93@gmail.com>
2016-12-21 17:07:49 +0100

    start synchronization code

--- src/Util/Logger.java
+++ src/Util/Logger.java
@@ -2,11 +2,16 @@ package Util;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Logger {
 	private File logFile;
 	
+	
+	private List<LogLineStorage> writeBuffer = new ArrayList<>();
+
 	private static File logFileStatic;
 	
 	private Logger(){
@@ -23,20 +28,49 @@ public class Logger {
 		logFileStatic = f;
 	}
 	
-	public void Log(String toLog,LogLevel lvl, long currentTime){
+	public synchronized void Log(String toLog,LogLevel lvl, long currentTime,String who){
+		LogLineStorage lls = new LogLineStorage(toLog,lvl,currentTime,who);
+		writeBuffer.add(lls);
+	}
+	private void writeToLog(String toLog,LogLevel lvl, long currentTime,String who){
 		if(logFile == null)
 			return;
 		
 		Date time = new Date(currentTime);
 		
-		String output = "[" + time + "] " + lvl + " | " + toLog;
+		String output = "["+time+"] "+who+" "+lvl +" # "+toLog;
 		
 		try {
 			FileUtil.writeToFile(output, logFile);
 		} catch (IOException e) {
 			e.printStackTrace();
-		}			
+		}	
 	}
+	private class LogLineStorage{
+		private String toLog;
+		private LogLevel lvl;
+		private long time;
+		private String who;
 		
+		public LogLineStorage(String toLog, LogLevel lvl, long time,String who){
+			this.toLog = toLog;
+			this.lvl = lvl;
+			this.time = time;
+			this.who = who;
+		}
+		
+		public String getToLog(){
+			return toLog;
+		}
+		public LogLevel getLvl(){
+			return lvl;
+		}
+		public long getTime(){
+			return time;
+		}
+		public String getWho(){
+			return who;
+		}
+	}
 	
 }
