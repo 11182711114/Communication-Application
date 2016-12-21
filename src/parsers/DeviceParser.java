78a129a470bd97f4ba400b558d6312869917f61d
Fredrik <flarsson93@gmail.com>
2016-12-21 12:04:19 +0100

    throws IllegalArgumentException
    throws IllegalArgumentException if the string supplied does not contain
    "scan" in second field

--- src/parsers/DeviceParser.java
+++ src/parsers/DeviceParser.java
@@ -5,7 +5,7 @@ import java.net.UnknownHostException;
 import Application.Device;
 
 public interface DeviceParser {
-	Device parse(String s) throws UnknownHostException;
-	Device parse(String deviceId, String s) throws UnknownHostException;
+	Device parse(String s) throws UnknownHostException, IllegalArgumentException;
+	Device parse(String deviceId, String s) throws UnknownHostException, IllegalArgumentException;
 
 }
--- src/parsers/NmapParser.java
+++ src/parsers/NmapParser.java
@@ -10,16 +10,20 @@ public class NmapParser implements DeviceParser{
 	
 	
 	@Override
-	public Device parse(String s) throws UnknownHostException {
+	public Device parse(String s) throws UnknownHostException, IllegalArgumentException {
 		String[] tmp = s.split(" ");
+		if(tmp[1] != "scan")
+			throw new IllegalArgumentException("Not a scan string");
 		InetAddress ip = InetAddress.getByName(tmp[5]);
 		Device d = new Device(ip);
 		return d;
 	}
 
 	@Override
-	public Device parse(String deviceID, String s) throws UnknownHostException {
+	public Device parse(String deviceID, String s) throws UnknownHostException, IllegalArgumentException {
 		String[] tmp = s.split(" ");
+		if(tmp[1] != "scan")
+			throw new IllegalArgumentException("Not a scan string");
 		InetAddress ip = InetAddress.getByName(tmp[5]);
 		Device d = new Device(deviceID,ip);
 		return d;
