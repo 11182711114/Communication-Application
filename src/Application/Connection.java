d3d96fd7480346f46279c44e834bf8e92e31a833
Axel <lexa.andersson@gmail.com>
2016-12-21 13:07:59 +0100

    Made simple read method in Con2

--- src/Application/Connection.java
+++ src/Application/Connection.java
@@ -7,66 +7,107 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Connection {
+public class Connection 
+{
 	Socket iSocket;
 	ServerSocket sSocket;
 	
-	public Connection(Socket s) {
+	public Connection(Socket s) 
+	{
 		iSocket = s;
 	}
 
-	public Connection(ServerSocket s) {
-			sSocket = s;
+	public Connection(ServerSocket s) 
+	{
+		sSocket = s;
 	}
 
-	public void connect() {
+	public void connect() 
+	{
 		
 	}
-	public void listenServerSocket(){
-		if(sSocket != null){
-			try {
+	
+	public void listenServerSocket()
+	{
+		if(sSocket != null)
+		{
+			try
+			{
 				System.out.println("Listening on port: " + sSocket.getLocalPort());
 				iSocket = sSocket.accept();
-			} catch (IOException e) {e.printStackTrace();}
+			} catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
 		}
 		outputTestSocket();
 	}
-	private void outputTestSocket() {
-		while(true){
-			try {
+	
+	private void outputTestSocket() 
+	{
+		while(true)
+		{
+			try 
+			{
 				Thread.sleep(5000);
-			} catch (InterruptedException e) {e.printStackTrace();}
+			} 
+			catch (InterruptedException e) 
+			{
+				e.printStackTrace();
+			}
+			
 			String toPrint = receive();
 			System.out.println(toPrint);
 		}
 	}
 
-	public void send(String string) {
-		try {
+	public void send(String string) 
+	{
+		try 
+		{
 			PrintWriter output = new PrintWriter(iSocket.getOutputStream(),true);
 			output.println(string);
-		} catch (IOException e) {e.printStackTrace();}
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 	}
-	public String receive(){
+	
+	public String receive()
+	{
 		String output = "";
 		
-		try {
+		try 
+		{
 			if(iSocket == null)
-				System.out.println("Null inputstream!");
+			System.out.println("Null inputstream!");
+			
 			BufferedReader input = new BufferedReader(new InputStreamReader(iSocket.getInputStream()));
 			System.out.println("Waiting for data on socket:" + iSocket.getLocalPort());
+			
 			output = input.readLine();
-		} catch (IOException e) {e.printStackTrace();}
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 		
 		return output;
 	}
 
-	public void close() {
-		try{
+	public void close() 
+	{
+		try
+		{
 			if(sSocket != null)
 				sSocket.close();
 			if(iSocket != null)
 				iSocket.close();
-		}catch(IOException e){e.printStackTrace();}
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 }
