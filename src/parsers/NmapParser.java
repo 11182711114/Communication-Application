7ae6cf4cfb6be9028aeb633051cdac3d69dff914
Fredrik <Fredrik@DESKTOP-QPEG16D>
2016-12-24 00:39:42 +0100

    Fix NmapParser logic

--- src/parsers/NmapParser.java
+++ src/parsers/NmapParser.java
@@ -4,26 +4,42 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 import Application.Device;
+import Util.Logger;
 
 public class NmapParser implements DeviceParser {
+	private Logger log = Util.Logger.getInstance();
+	private String nameForLog = this.getClass().getSimpleName();
+	
+	String relLine = "Nmap scan report for ";
 
+	/**
+	 * Parses a string from Nmap into Device
+	 * 
+	 * @param s
+	 * The string to be parsed
+	 * @return a Device with the given IP
+	 * @throws IllegalArgumentException
+	 * if the given string cannot be parsed
+	 */
 	@Override
 	public Device parse(String s) throws UnknownHostException, IllegalArgumentException {
-		String[] tmp = s.split(" ");
-		if (tmp[1] != "scan")
-			throw new IllegalArgumentException("Not a scan string");
-		InetAddress ip = InetAddress.getByName(tmp[5]);
+		log.debug("Trying to parse "+s+ " into Device", nameForLog);
+		if(!s.contains(relLine))
+			throw new IllegalArgumentException("The given string\""+ s + "\" cannot be parsed into a valid device" );
+		
+		InetAddress ip = InetAddress.getByName(s.replace(relLine, ""));
 		Device d = new Device(ip);
 		return d;
 	}
 
 	@Override
 	public Device parse(String deviceID, String s) throws UnknownHostException, IllegalArgumentException {
-		String[] tmp = s.split(" ");
-		if (tmp[1] != "scan")
-			throw new IllegalArgumentException("Not a scan string");
-		InetAddress ip = InetAddress.getByName(tmp[5]);
-		Device d = new Device(deviceID, ip);
+		log.debug("Trying to parse "+s+ " into Device with deviceID: "+ deviceID, nameForLog);
+		if(!s.contains(relLine))
+			throw new IllegalArgumentException("The given string\""+ s + "\" cannot be parsed into a valid device" );
+		
+		InetAddress ip = InetAddress.getByName(s.replace(relLine, ""));
+		Device d = new Device(ip,deviceID);
 		return d;
 	}
 
