96d8ca15c9e97677ccc9e2cb15f9184e6ed7832a
Fredrik <Fredrik@DESKTOP-QPEG16D>
2016-12-26 21:37:19 +0100

    added default monitor folder

--- src/Application/CommunicationApplication.java
+++ src/Application/CommunicationApplication.java
@@ -14,8 +14,9 @@ public class CommunicationApplication {
 	//PROGRAM OPTIONS - DEFAULT VALUES
 	private int listenPort = 10231; // "-port X"
 	private boolean continuous = false; // "-c" = true
-	private File logLocation = new File("./db/");
+	private File logDir = new File("./db/");
 	private String logName = "ComApp.log";
+	private File monitorDir = new File("./monitor/");
 
 	private ChannelHandler cH;
 
@@ -41,7 +42,7 @@ public class CommunicationApplication {
 					break;
 					
 				case "-logLoc":
-					logLocation = new File(args[i+1]);
+					logDir = new File(args[i+1]);
 					break;
 					
 				case "-logName":
@@ -58,12 +59,16 @@ public class CommunicationApplication {
 						pathNameSep = str.lastIndexOf("\\");
 					
 					String loc = str.substring(0, pathNameSep+1);
-					logLocation = new File(loc);
+					logDir = new File(loc);
 					logName = str.substring(pathNameSep);
 					break;					
 					
 				case "-port":
 					listenPort = Integer.parseInt(args[i+1]);
+					break;
+					
+				case "-mon":
+					monitorDir = new File(args[i+1]);
 					break;
 
 //					old manual stuff
@@ -98,12 +103,12 @@ public class CommunicationApplication {
 		}
 	}
 	private void startLogger(){
-		Util.Logger.setLogFile(new File(logLocation.getPath()+File.separator+logName));
+		Util.Logger.setLogFile(new File(logDir.getPath()+File.separator+logName));
 		log = Util.Logger.getInstance();
 		log.start();		
 	}
 	private void startContinuousOperation() {
-		cH = new ChannelHandler(new HashSet<Channel>(), new LinkedList<Channel>());
+		cH = new ChannelHandler(new HashSet<Channel>(), new LinkedList<Channel>(),monitorDir);
 		try {
 			cH.setPortListener(new PortListener(cH, new ServerSocket(listenPort)));
 		} catch (IOException e) {
