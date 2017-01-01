9b84375d8c097df6dfb59af3464c3e6bd0264348
Fredrik <Fredrik@DESKTOP-QPEG16D>
2017-01-01 22:02:46 +0100

    size+1 -> size+2

--- src/discovery/RoutingTableOutputGenerator.java
+++ src/discovery/RoutingTableOutputGenerator.java
@@ -9,7 +9,7 @@ public class RoutingTableOutputGenerator {
 	public static String[] generateRoutingTableAsStringArray(RoutingTable rt) {
 		List<Device> devices = rt.getRouting();
 		
-		String[] tmp = new String[devices.size()+1];
+		String[] tmp = new String[devices.size()+2];
 		tmp[0] = "START";
 		for (int i = 0; i < tmp.length; i++) {
 			tmp[i] = devices.get(i).toPrint();
