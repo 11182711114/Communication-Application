f0faa194fee716aaba232e97542583c2de901cde
Fredrik <flarsson93@gmail.com>
2017-01-03 12:18:22 +0100

    remove debugging

--- src/util/DeviceIdExtractor.java
+++ src/util/DeviceIdExtractor.java
@@ -11,17 +11,8 @@ public class DeviceIdExtractor {
 	 * @throws IOException
 	 */
 	public static String extractFromFolder(File parent, File folder) throws IOException{
-		System.out.println(parent.getAbsolutePath());
-		System.out.println(folder.getAbsolutePath());
 		String[] parentPath = parent.getAbsolutePath().split(File.separator);
-		for(String s : parentPath){
-			System.out.print(s + ",");
-		}
-		System.out.println();
 		String[] folderPath = folder.getAbsolutePath().split(File.separator);
-		for(String s : folderPath){
-			System.out.print(s + ",");
-		}
 		String output = folderPath[parentPath.length];
 		
 		return output;
