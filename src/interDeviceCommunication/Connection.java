8e9210e6e4ccd2c7fa205a893b11c79a60ef7074
Fredrik <flarsson93@gmail.com>
2017-01-03 14:48:58 +0100

    logging

--- src/interDeviceCommunication/Connection.java
+++ src/interDeviceCommunication/Connection.java
@@ -74,7 +74,7 @@ public class Connection implements Runnable {
 
 	public void send(OutputDataPacket[] packets) {
 		try {
-			log.debug("Checking if socket is conneced: " + socket.isConnected());
+			log.debug("Checking if socket is conneced before writing: " + socket.isConnected());
 			BufferedWriter output = new BufferedWriter(new PrintWriter(socket.getOutputStream(), true));
 			for (OutputDataPacket p : packets) {
 				String[] data = p.toSend();
