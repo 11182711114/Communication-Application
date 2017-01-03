c85ea34b4ad7effbdf57f3ae931d08eb35de2e06
Fredrik <flarsson93@gmail.com>
2017-01-03 18:56:14 +0100

    update script

--- scripts/caHelper.py
+++ scripts/caHelper.py
@@ -16,24 +16,24 @@ if len(args) > 1:
 	for index in range(len(args)):
 		arg = args[index]
 		if arg == "-v": #verbose
-			outputChan = None
+				outputChan = None
 		elif arg == "-q": #quiet
-			with open(os.devnull, 'w') as output:
-				sys.stdout = output
+				with open(os.devnull, 'w') as output:
+						sys.stdout = output
 		elif arg == "-b": #branch
-			branch = args[index+1]
+				branch = args[index+1]
 		elif arg == "-pU":#project URL root
-			projectRoot = args[index + 1]
+				projectRoot = args[index + 1]
 		elif arg == "-CP":#class path, e.g. "src/**/*.java"
-			compilePath = args[index+1]
+				compilePath = args[index+1]
 		elif arg == "--help" or arg == "-h":
-			print(usage)
+				print(usage)
 		elif arg == "-o":#output directory
-			outputDir = args[index+1]
+				outputDir = args[index+1]
 
 if projectRoot.rindex('/') == len(projectRoot)-1:
-	projectRootTMP = projectRoot[:len(projectRoot)-2]
-	projectRoot = projectRootTMP
+        projectRootTMP = projectRoot[:len(projectRoot)-2]
+        projectRoot = projectRootTMP
 
 githubArchiveRelative = "/archive/"
 githubStandardFileExtension = ".zip"
@@ -42,30 +42,30 @@ fileLoc = projectRoot + githubArchiveRelative + branch + githubStandardFileExten
 print("Downloading archive")
 wget_return = subprocess.call("wget "+fileLoc, shell=True, stdout=outputChan, stderr=subprocess.STDOUT)
 if wget_return != 0:
-	print("Something went wrong downloading")
-	sys.exit()
+        print("Something went wrong downloading")
+        sys.exit()
 print("Download successful")
 
 archivePath = currentDir+branch+githubStandardFileExtension
 print("Trying to unzip " + archivePath)
 unzip_return = subprocess.call("unzip -o " + archivePath, shell=True, stdout=outputChan, stderr=subprocess.STDOUT)
 if unzip_return != 0:
-	print("Something went wrong unzipping, exiting")
-	sys.exit()
+        print("Something went wrong unzipping, exiting")
+        sys.exit()
 print("unzipping successful")
 
 print("Checking if " + outputDir + " exists")
-if not os.path.exists(outputDir):
-	print("making directory")
-	os.makedirs(outputDir)
+if not os.path.exists(outputDir) and outputDir != "./":
+        print("making directory")
+        os.makedirs(outputDir)
 
-compileRoot = currentDir + projectRoot[projectRoot.rindex('/'):] + branch
-compilingDir = compileRoot + compilePath
+compileRoot = currentDir + projectRoot[projectRoot.rindex('/')+1:] +"-"+ branch
+compilingDir = compileRoot + "/" + compilePath
 print("Compiling: javac -d " + outputDir +" "+ compilingDir)
 javac_return = subprocess.call("javac -d " + outputDir +" "+ compilingDir, shell=True, stdout=outputChan, stderr=subprocess.STDOUT)
 if javac_return != 0:
-	print("Something went wrong compiling")
-	sys.exit()
+        print("Something went wrong compiling")
+        sys.exit()
 print("Compiling complete")
 
 print("Cleaning: "+ archivePath)
